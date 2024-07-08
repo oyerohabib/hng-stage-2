58e708dc39d69a3a2db6a117e611b24df3efc6e4
oyerohabib <oyerohabib@yahoo.com>
2024-07-08 14:27:51 +0100

    feat: setup tailwindcss for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 function App() {
   return (
-    <>
-      <div>Hello World</div>
-    </>
+    <div className="text-center">
+      <header className="bg-blue-500 text-white p-4">
+        <h1 className="text-3xl font-bold">
+          Welcome to React with Tailwind CSS
+        </h1>
+      </header>
+    </div>
   );
 }
 
