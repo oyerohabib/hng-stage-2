94ae0f38dc625ab85a9d5d1bbad012d6977e94ed
oyerohabib <oyerohabib@yahoo.com>
2024-07-10 15:47:07 +0100

    feat: changed fmr mobile nav to use correct nav as in design

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,26 +1,20 @@
-import { useState } from "react";
 import { IoMdSearch } from "react-icons/io";
 import { RiHome2Line } from "react-icons/ri";
 import { MdOutlineShoppingCart } from "react-icons/md";
-import { FaBars, FaRegHeart } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function Header() {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
   return (
     <>
       <header className="bg-white">
         <div className="container mx-auto px-4 py-8 flex justify-between items-center">
-          <h1 className="text-4xl font-bold text-black">
+          <h1 className="text-2xl md:text-4xl font-bold text-black">
             <Link to="/">
               Beauty<span className="text-yellow-600">Home</span>
             </Link>
           </h1>
-          <div className="items-center border border-gray-300 lg:w-1/2 hidden md:flex">
+          <div className="hidden md:flex items-center border border-gray-300 lg:w-1/2">
             <input
               type="text"
               placeholder="Search products"
@@ -47,38 +41,28 @@ export default function Header() {
               <MdOutlineShoppingCart className="text-base text-yellow-600" />
               <span>Cart</span>
             </Link>
-            <button onClick={toggleMobileMenu} className="md:hidden">
-              <FaBars className="text-yellow-600 text-base" />
-            </button>
+            <Link
+              to="/wishlist"
+              className="flex md:hidden items-center space-x-1"
+            >
+              <FaRegHeart className="text-2xl text-yellow-600" />
+            </Link>
+            <Link to="/cart" className="flex md:hidden items-center space-x-1">
+              <MdOutlineShoppingCart className="text-2xl text-yellow-600" />
+            </Link>
           </div>
         </div>
         <div className="bg-yellow-300 divide-y h-[1px] mb-6"></div>
-        <div
-          className={`md:hidden bg-white overflow-hidden transition-all duration-300 ease-in-out ${
-            isMobileMenuOpen ? "max-h-40 opacity-100" : "max-h-0 opacity-0"
-          }`}
-        >
-          <Link
-            to="/"
-            className="block px-4 py-2"
-            onClick={() => setIsMobileMenuOpen(false)}
-          >
-            Home
-          </Link>
-          <Link
-            to="/wishlist"
-            className="block px-4 py-2"
-            onClick={() => setIsMobileMenuOpen(false)}
-          >
-            Wishlist
-          </Link>
-          <Link
-            to="/cart"
-            className="block px-4 py-2"
-            onClick={() => setIsMobileMenuOpen(false)}
-          >
-            Cart
-          </Link>
+        <div className="flex md:hidden items-center border border-gray-300 px-4 mx-4 xs:mx-8 mb-6">
+          <input
+            type="text"
+            placeholder="Search products"
+            className="flex-grow px-4 py-2 focus:outline-none"
+          />
+          <div className="w-px h-6 bg-gray-300"></div>
+          <div className="p-2 text-gray-500">
+            <IoMdSearch className="text-yellow-600 text-2xl" />
+          </div>
         </div>
       </header>
     </>
