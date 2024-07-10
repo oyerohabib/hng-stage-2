7f6494a23d820e4a1204cf98376d0b78eef16f1a
oyerohabib <oyerohabib@yahoo.com>
2024-07-10 02:19:25 +0100

    feat: added all product enteries

---
+++ src/Constants/Products.js
@@ -0,0 +1,102 @@
+export const Products = [
+  {
+    name: "Collagen Super Powder",
+    image: "../../assets/ProductItems/collagenpowder.jpg",
+    price: "N 4,999",
+  },
+  {
+    name: "CeraVe Acne Control Cleanser",
+    image: "../../assets/ProductItems/cream.jpg",
+    price: "N 7,999",
+  },
+  {
+    name: "Morocan Super Black Soap",
+    image: "../../assets/ProductItems/crown.jpg",
+    price: "N 7,520",
+  },
+  {
+    name: "Venus Eye Pencil Collection 4-in-1 Pack",
+    image: "../../assets/ProductItems/pencils.jpg",
+    price: "N 3,999",
+  },
+  {
+    name: "Curl Expression Lotion Cream",
+    image: "../../assets/ProductItems/curlcream.jpg",
+    price: "N 4,999",
+  },
+  {
+    name: "Dettol Skin protection Soap - 160g",
+    image: "../../assets/ProductItems/dettol.jpg",
+    price: "N 1,599",
+  },
+  {
+    name: "Oxygen Plasma Liquids Drops",
+    image: "../../assets/ProductItems/dropper.jpg",
+    price: "N 7,880",
+  },
+  {
+    name: "Curl Expression Lotion Cream",
+    image: "../../assets/ProductItems/curlcream.jpg",
+    price: "N 4,999",
+  },
+  {
+    name: "Venus Eye Lashes Collection 4-in-1 Pack",
+    image: "../../assets/ProductItems/eyebrow.jpg",
+    price: "N 4,999",
+  },
+  {
+    name: "Whipped Oxygen Moisturizing Cream",
+    image: "../../assets/ProductItems/whitedropper.jpg",
+    price: "N 2,999",
+  },
+  {
+    name: "Allure Cream Conditioner + Shampoo",
+    image: "../../assets/ProductItems/haircream.jpg",
+    price: "N 4,999",
+  },
+  {
+    name: "Foot Massage Syetem + Extra face",
+    image: "../../assets/ProductItems/hairwasher.jpg",
+    price: "N 5,999",
+  },
+  {
+    name: "Loreal Shampooing - Silver",
+    image: "../../assets/ProductItems/loreal.jpg",
+    price: "N 6,999",
+  },
+  {
+    name: "Miracle Shampoo + Conditioner",
+    image: "../../assets/ProductItems/miracleshampoo.jpg",
+    price: "N 10,999",
+  },
+  {
+    name: "Nivea Perfect and Radiant Body Cream",
+    image: "../../assets/ProductItems/nivea.jpg",
+    price: "N 9,999",
+  },
+  {
+    name: "Venus Eye Pencil Collection 4-in-1 Pack",
+    image: "../../assets/ProductItems/pencils.jpg",
+    price: "N 3,999",
+  },
+  {
+    name: "EAU DE PARFUM Body Natural Spray",
+    image: "../../assets/ProductItems/perfume.jpg",
+    price: "N 4,999",
+  },
+  {
+    name: "Manual hair remover + Extra tooth",
+    image: "../../assets/ProductItems/shavingstick.jpg",
+    price: "N 5,999",
+  },
+  {
+    name: "Balancing Stray Toner Cream",
+    image: "../../assets/ProductItems/spraytoner.jpg",
+    price: "N 5,999",
+  },
+  {
+    name: "Whipped Oxygen Moisturizing Cream",
+    image: "../../assets/ProductItems/whitedropper.jpg",
+    price: "N 2,999",
+  },
+];
