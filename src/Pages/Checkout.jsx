7aca2d5038be3059983e470e7dff56bb9e5d9f16
oyerohabib <oyerohabib@yahoo.com>
2024-07-10 16:24:23 +0100

    fix: fixed responsiveness issues in checkout page

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -26,10 +26,10 @@ const CheckoutPage = () => {
         </nav>
         <h1 className="text-5xl mb-6">Checkout</h1>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-          <div className="lg:col-span-2 space-y-4">
+          <div className="lg:col-span-2 space-y-4 sm:order-first order-last">
             <div className="bg-white py-6 rounded shadow-sm">
               <h2 className="text-xl font-bold mb-4">Select Payment Method</h2>
-              <div className="grid grid-cols-2 gap-4">
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div className="border rounded p-4">
                   <input
                     type="radio"
