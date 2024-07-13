2009683787149d8da2051c87e15ab29a492b35fc
oyerohabib <oyerohabib@yahoo.com>
2024-07-14 00:07:57 +0100

    feat: added loading state while fetching data

---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center mt-20">
+      <div className="animate-spin rounded-full h-20 w-20 border-b-2 border-gray-900"></div>
+    </div>
+  );
+};
+
+export default Spinner;
